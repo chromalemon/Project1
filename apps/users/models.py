6b7a906b07379b9223d9e2db6bfe64f2469e4146
chromalemon <19nmelnichenko@chelsea-academy.org>
2024-12-18 23:24:14 +0000

    Created models

--- apps/users/models.py
+++ apps/users/models.py
@@ -2,12 +2,34 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, AbstractBaseUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
 
-class CustomUser():
+class CustomUser(AbstractUser):
+    subjects_choices = {
+        "MA": "Mathematics",
+        "FM": "Further Mathematics",
+        "CS": "Computer Science",
+        "PH": "Physics",
+    }
+    subjects = models.CharField(max_length=128, choices=subjects_choices)
     school = models.CharField(_("school name"),max_length=128)
 
 
-class TeacherUser(CustomUser):
-    pass
+class Classroom(models.Model):
+    room_num = models.IntegerField(default=0)
+    seats_num = models.IntegerField(default=0)
+    room_type = models.CharField(max_length=64)
+    teacher_users = models.ManyToManyField(CustomUser, related_name="teacher_users")
+    student_users = models.ManyToManyField(CustomUser, related_name="student_users")
+    available = models.BooleanField(blank=True)
 
-class StudentUser(CustomUser):
-    pass 
+class Teacher(CustomUser):
+    authority_choices = {
+        1: "Teacher",
+        2: "Deputy Head",
+        3: "Head",
+    }
+    authority = models.IntegerField(default=1, choices=authority_choices)
+    main_class = models.IntegerField(default=None, blank=True)
+
+class Student(CustomUser):
+    entry_year = models.IntegerField(default=None)
+    prefect_status = models.BooleanField(default=False)
--- config/settings/base.py
+++ config/settings/base.py
@@ -64,6 +64,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "users.CustomUser"
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
